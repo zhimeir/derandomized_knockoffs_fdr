033abf1c9be06afe6f7878415c8018b21d9cdb4f
zhimeir <zren@sh03-ln06.stanford.edu>
2023-08-04 12:26:21 -0700

    sync

--- simulations/simulation_original_low.R
+++ simulations/simulation_original_low.R
@@ -9,7 +9,7 @@ if(is.na(amp)) amp <- 30
 suppressPackageStartupMessages(library(glmnet))
 suppressPackageStartupMessages(library(knockoff))
 suppressPackageStartupMessages(library(tidyverse))
-source("utils.R")
+source("../utils/utils.R")
 
 ## The directory to save the results
 save_dir <- sprintf("../results/simulation_original_low")
@@ -25,6 +25,7 @@ k <- 60
 alpha <- 0.1
 rho <- 0
 M <- 50
+mu <- rep(0,p)
 Sigma <- toeplitz(rho^(0:(p-1)))
 nonzero <- sample(1:p, k, replace = FALSE)
 beta_true <- amp * sign(rnorm(p)) * (1:p %in% nonzero) / sqrt(n)
@@ -40,24 +41,23 @@ Y <- y.sample(X)
 
 
 ## Vanilla  knockoff
-Xk <- create.gaussian(X, rep(0,p), Sigma, diag_s = diags)
+Xk <- create.gaussian(X, mu, Sigma, diag_s = diags)
 W <- stat.glmnet_coefdiff(X, Xk, Y)
 tau <- knockoff.threshold(W, fdr = alpha, offset = 1)
 rej <- which(W >= tau)
 fdp <- sum(beta_true[rej]==0) / max(length(rej), 1)
-power <- sum(beta_true[rej]!=0)/max(k,1)
+power <- sum(beta_true[rej]!=0) / k
 all_res <- rbind(all_res,
   data.frame(method = "vanilla", power = power, fdp = fdp))
 
 ## Compute knockoff e-values 
-res <- ekn(X, Y, M, gamma_list, Sigma, diags, "gaussian", offset = 1)
-E <- res$E_early
-res <- ebh(E, alpha)
-rej <- res$rej
+res <- ekn(X, Y, M, alpha/2, mu, Sigma, diags, "gaussian", offset = 1)
+E <- res$E
+rej <- ebh(E, alpha)$rej
 fdp <- sum(beta_true[rej]==0) / max(length(rej), 1)
-power <- sum(beta_true[rej]!=0) / max(k,1)
+power <- sum(beta_true[rej]!=0) / k
 all_res <- rbind(all_res,
-    data.frame(method = "multiple", power = power, fdp = fdp, seedB = seedB))
+    data.frame(method = "multiple", power = power, fdp = fdp))
 
 out_dir <- sprintf("%s/res_amp_%d_seedA_%d.csv", save_dir, amp, seedA)
 write_csv(all_res, out_dir)
